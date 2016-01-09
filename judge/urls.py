2e3c4f02d192db391d0a1eb0c78ec44eb62389cf
ad5915 <ad5915@imperial.ac.uk>
2016-01-09 20:53:45 +0000

    Added dummy question search and display.

--- judge/urls.py
+++ judge/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from judge.views.date_views import date_time, hours_ahead
-from judge.views.basic_views import display_home, no_page
+
+from judge.views.basic_views import display_home
+from judge.views.basic_views import display_questions
+from judge.views.basic_views import no_page
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^$', display_home),
     
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^questions/$', display_questions),
     
     url(r'.+', no_page),
 )
--- judge/views/basic_views.py
+++ judge/views/basic_views.py
@@ -1,6 +1,10 @@
 from django.template.loader import get_template
 from django.template import Context
 
+from django.db.models import Q
+from django.shortcuts import render_to_response
+from judge.database.models import Question
+
 from django.http import HttpResponse
 
 def display_home(request):
@@ -11,3 +15,26 @@ def display_home(request):
 def no_page(req):
 	html = "<html><body>No page<body><html>"
 	return HttpResponse(html)
+
+def display_questions(request):
+	query = request.GET.get('q', '')
+	all_results = 0
+	if query:
+		qset = (
+			Q(user__icontains=query) |
+			Q(title__icontains=query) |
+			Q(content__icontains=query)
+		)
+		results = Question.objects.filter(qset).distinct()
+	else:
+		results = []
+		all_results = Question.objects.distinct()
+	if all_results:
+		return render_to_response('questions.html', {
+			"results": all_results,
+		})
+	else:
+		return render_to_response('questions.html', {
+			"results": results,
+			"query": query
+		})
