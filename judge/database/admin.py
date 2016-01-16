165b07665d6fd1c7be051a31bc73d750396a7ed9
ad5915 <ad5915@imperial.ac.uk>
2016-01-16 22:18:08 +0000

    Modifed database.

--- judge/database/admin.py
+++ judge/database/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from judge.database.models import Question
 from judge.database.models import Task
 from judge.database.models import Article
+from judge.database.models import Hack
 
 admin.site.register(Question)
 admin.site.register(Article)
 admin.site.register(Task)
+admin.site.register(Hack)
