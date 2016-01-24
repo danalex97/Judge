258a64137e77fc5d5fdcf9e2d629965c712463f2
ad5915 <ad5915@imperial.ac.uk>
2016-01-24 19:46:45 +0000

    Added dummy external files.

--- judge/static/js/display.js
+++ judge/static/js/display.js
@@ -1,7 +1,32 @@
-function hello() {
+function deps() {
+	var str = 
+	' <head> ' +
+	' 	<link id="hdep0" rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css" media="screen"> ' +
+	' </head>' +
+	' <body> ' +
+	'   <script id="hdep1" src="/static/js/bootstrap.min.js"></script> ' +
+	'   <script id="hdep2" src="/static/js/display.js"></script> ' +
+	'   <script id="hdep3" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> ' +
+	'   <script id="hdep4" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> ' +
+	' </body> ';
+	return str;
+}
+
+function appendById(d, id) {
+	var cluster = document.getElementById(id);
+	var content = cluster.innerHTML;
+	
+	d.write( content );
+}
+
+function display(id) {
 	var w = window.open();
 	var d = w.document;
+	
 	d.open();
-	d.write("<p>Hello World</p>");
+	
+	d.write(deps());
+	appendById(d, id);
+	
 	d.close();
 } 
